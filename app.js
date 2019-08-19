056955adfa31156507045b011ef4ff9dad4030cd
xiaofan <1353815633@qq.com>
2019-08-19 23:20:43 +0800

    koa-router

--- app.js
+++ app.js
@@ -2,37 +2,30 @@
  * @Author: DevZhang 
  * @Date: 2019-08-15 23:26:07 
  * @Last Modified by: DevZhang
- * @Last Modified time: 2019-08-19 23:01:45
+ * @Last Modified time: 2019-08-19 23:20:55
  */
 
 
-const Koa = require('koa')
-const app = new Koa()
+const Koa = require('koa');
+const app = new Koa();
+// require koa-router 返回的函数
+const router = require('koa-router')();
 
-app.use(async (ctx, next) => {
-    if (ctx.request.path === '/') {
-        ctx.response.body = '<h1>index page</h1>';
-    } else {
-        await next();
-    }
-});
+// 添加路由
+router.get('/', async (ctx, next) => {
+    ctx.response.body = `<h1>index page</h1>`
+})
 
-app.use(async (ctx, next) => {
-    if (ctx.request.path === '/home') {
-        ctx.response.body = '<h1>home page</h1>';
-    } else {
-        await next();
-    }
-});
+router.get('/home', async (ctx, next) => {
+    ctx.response.body = `<h1>home home</h1>`
+})
 
-app.use(async (ctx, next) => {
-    if (ctx.request.path === '/404') {
-        ctx.response.body = '<h1>404 Not Found</h1>';
-    } else {
-        await next();
-    }
-});
+router.get('/404', async (ctx, next) => {
+    ctx.response.body = `<h1>404 404</h1>`
+})
 
+// 调用路由中间件
+app.use(router.routes())
 
 app.listen(3000, () => {
     console.log('[demo] start-quick is starting at port 3000')
