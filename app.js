3108049005b227b740a605eb2f0e014090422476
xiaofan <1353815633@qq.com>
2019-08-19 23:06:32 +0800

    router one

--- app.js
+++ app.js
@@ -2,22 +2,38 @@
  * @Author: DevZhang 
  * @Date: 2019-08-15 23:26:07 
  * @Last Modified by: DevZhang
- * @Last Modified time: 2019-08-16 21:38:19
+ * @Last Modified time: 2019-08-19 23:01:45
  */
 
 
 const Koa = require('koa')
 const app = new Koa()
 
-// app.use(async (ctx) => {
-//     ctx.body = 'hello koa2'
-// })
+app.use(async (ctx, next) => {
+    if (ctx.request.path === '/') {
+        ctx.response.body = '<h1>index page</h1>';
+    } else {
+        await next();
+    }
+});
 
 app.use(async (ctx, next) => {
-    await next()
-    ctx.response.type = 'text/html'
-    ctx.response.body = '<h1>你好, 亲爱的小明同学</h1>'
-})
+    if (ctx.request.path === '/home') {
+        ctx.response.body = '<h1>home page</h1>';
+    } else {
+        await next();
+    }
+});
 
-app.listen(3000)
-console.log('[demo] start-quick is starting at port 3000')
+app.use(async (ctx, next) => {
+    if (ctx.request.path === '/404') {
+        ctx.response.body = '<h1>404 Not Found</h1>';
+    } else {
+        await next();
+    }
+});
+
+
+app.listen(3000, () => {
+    console.log('[demo] start-quick is starting at port 3000')
+})
